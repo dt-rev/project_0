92ae481f32b04ee6da8b1b50731a82fe8a4b101a
Duncan Thomas <thomas@mymail.mines.edu>
2021-04-27 21:15:31 -0500

    added game-listing menu option with sorting functionality

--- src/main/scala/com/revature/proj0/cli/Cli.scala
+++ src/main/scala/com/revature/proj0/cli/Cli.scala
@@ -7,6 +7,10 @@ import com.revature.proj0.utils.ConnectionUtil
 class Cli {
     val commandArgPattern: Regex = "(\\w+)\\s*(.*)".r
 
+    val db = "jdbc:postgresql://localhost:5432/project0"
+    val user = "postgres"
+    val pass = "postgres"
+
     def menu(): Unit = {
         printWelcome()
         var continueMenuLoop = true
@@ -15,22 +19,80 @@ class Cli {
 
             var input = StdIn.readLine()
             input match {
-                case commandArgPattern(cmd, arg) if cmd.equalsIgnoreCase("test") => {
-                    println("testing db connection")
-                    val conn = ConnectionUtil.getConnection("jdbc:postgresql://localhost:5432/chinook",
-                                                                "postgres", "postgres")
+                case commandArgPattern(cmd, arg) if cmd.equalsIgnoreCase("g") => {
+                    println("\nWhat would you like to sort by?")
+                    println("T: Title")
+                    println("D: Developer")
+                    println("P: Publisher")
+                    println("E: ESRB rating")
+                    println("C: Console/Platform")
+                    println("R: Release date")
+
+                    var continueGamesLoop = false
+                    var orderBy = "title"
+                    do {
+                        input = StdIn.readLine()
+                        continueGamesLoop = false
+
+                        input.toLowerCase match {
+                            case "t" => orderBy = "g.title"
+                            case "d" => orderBy = "g.developer"
+                            case "p" => orderBy = "g.publisher"
+                            case "e" => orderBy = "g.rating"
+                            case "c" => orderBy = "gpr.platform_name_fk"
+                            case "r" => orderBy = "gpr.release_date"
+                            case _ => {
+                                println("Invalid option")
+                                continueGamesLoop = true
+                            }
+                        }
+
+                    } while (continueGamesLoop)
+
+                    
+
+                    // dao takes over here
+                    //val rs = dao.getGames(orderBy)
+                    val conn = ConnectionUtil.getConnection(db, user, pass)
+
+                    val stmt = conn.prepareStatement("select g.title, g.developer, g.publisher, g.rating, gpr.platform_name_fk, gpr.release_date " +
+                                                            "from games g left join games_platforms_releases gpr on g.title = gpr.game_title_fk " +
+                                                            s"order by ${orderBy};")
+
+                    stmt.execute()
+
+                    val rs = stmt.getResultSet() // dao should return the ResultSet I think
+                    
+                    println("\nGames:")
+                    println("\n[Title] - [Developer] - [Publisher] - [ESRB Rating] - [Platform] - [Release Date]")
+                    while(rs.next()) {
+                        println(s"${rs.getString("title")} - ${rs.getString("developer")} - ${rs.getString("publisher")} - " +
+                                s"${rs.getString("rating")} - ${rs.getString("platform_name_fk")} - ${rs.getString("release_date")}")
+                    }
+
+                    conn.close
+                }
+                
+                case commandArgPattern(cmd, arg) if cmd.equalsIgnoreCase("c") => {
+                    println("testing db connection...")
+                    val conn = ConnectionUtil.getConnection(db, user, pass)
                     val stmt = conn.prepareStatement("""SELECT * FROM information_schema.tables
                                                         WHERE table_schema = 'public';""")
                     stmt.execute()
 
                     val rs = stmt.getResultSet()
+                    println("\nDatabase Tables:")
                     while(rs.next()) {
                         println(rs.getString("table_name"))
                     }
+
+                    conn.close
                 }
-                case commandArgPattern(cmd, arg) if cmd.equalsIgnoreCase("exit") => {
+
+                case commandArgPattern(cmd, arg) if cmd.equalsIgnoreCase("q") => {
                     continueMenuLoop = false
                 }
+
                 case _ => {
                     println("Failed to parse command")
                 }
@@ -38,17 +100,17 @@ class Cli {
         } while (continueMenuLoop)
     }
 
-    // TODO: give the project a more accurate title
     def printWelcome(): Unit = {
-        println("PROJECT 0")
+        println("Video Game Database")
     }
 
     // TODO: make sure all menu options appear here
     def printMenuOptions(): Unit = {
         List(
             "\n----\nMENU\n----",
-            "test: test db connection",
-            "exit: quit the application",
+            "G: list of games",
+            "C: list of consoles/platforms",
+            "Q: quit the application",
             "----\n"
         ).foreach(println)
     }
